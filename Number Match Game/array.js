159f854efd9af29d7f8b438cda18ae53590754b0
chrisdixon161 <chrisdixon161@googlemail.com>
2020-07-16 12:05:37 +0100

    added number match array

---
+++ Number Match Game/array.js
@@ -0,0 +1,82 @@
+const images = [
+  {
+    image_name: 'bananas.jpg',
+    number_of_items: 6,
+  },
+  {
+    image_name: 'birthday candles.jpg',
+    number_of_items: 7,
+  },
+  {
+    image_name: 'blocks.jpg',
+    number_of_items: 6,
+  },
+  {
+    image_name: 'brushes.jpg',
+    number_of_items: 7,
+  },
+  {
+    image_name: 'cakes.jpg',
+    number_of_items: 7,
+  },
+  {
+    image_name: 'cars.jpg',
+    number_of_items: 2,
+  },
+  {
+    image_name: 'crayons.jpg',
+    number_of_items: 8,
+  },
+  {
+    image_name: 'cupcakes.jpg',
+    number_of_items: 7,
+  },
+  {
+    image_name: 'deer.jpg',
+    number_of_items: 3,
+  },
+  {
+    image_name: 'donuts.jpg',
+    number_of_items: 6,
+  },
+  {
+    image_name: 'ducks.jpg',
+    number_of_items: 6,
+  },
+  {
+    image_name: 'eggs.jpg',
+    number_of_items: 8,
+  },
+  {
+    image_name: 'elephants.jpg',
+    number_of_items: 7,
+  },
+  {
+    image_name: 'hot air balloons.jpg',
+    number_of_items: 5,
+  },
+  {
+    image_name: 'jelly beans.jpg',
+    number_of_items: 9,
+  },
+  {
+    image_name: 'macaroons.jpg',
+    number_of_items: 7,
+  },
+  {
+    image_name: 'pencils.jpg',
+    number_of_items: 12,
+  },
+  {
+    image_name: 'people.jpg',
+    number_of_items: 6,
+  },
+  {
+    image_name: 'peppers.jpg',
+    number_of_items: 2,
+  },
+  {
+    image_name: 'pizza slices.jpg',
+    number_of_items: 8,
+  },
+]
